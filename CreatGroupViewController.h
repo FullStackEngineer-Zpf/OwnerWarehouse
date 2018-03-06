740ad50734e2578cbb55114fa08f76219d886d45
mac <mac@macdeMacBook-Pro-2.local>
2018-03-06 16:27:47 +0800

    zhang

---
+++ CreatGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreatGroupViewController.h
+//  YouBang
+//
+//  Created by zhang on 16/7/11.
+//
+//
+
+#import "YBRootViewController.h"
+
+@interface CreatGroupViewController : YBRootViewController
+
+@end
